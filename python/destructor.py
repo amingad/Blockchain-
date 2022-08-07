6824e36993efae4c4f439495712eda38eb0fb57e
amingad <sachin.amingad@yahoo.com>
2022-08-07 16:58:18 +0530

    added destructor

---
+++ python/destructor.py
@@ -0,0 +1,12 @@
+class sachin :
+    def __init__(self):
+        print("Machine learning is getting added")
+       
+
+    def __del__(self):
+        print("Machine learning is getting destroyed")
+
+
+obj = sachin()
+del obj 
+
